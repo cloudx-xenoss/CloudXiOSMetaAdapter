331eb2e5df7a3e74791b2c4fac5b40b5e832ff88
Bryan Boyko <bryan.boyko@gmail.com>
2025-04-01 16:00:31 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CloudXMetaAdapter",
-            url: "https://github.com/cloudx-xenoss/CloudXiOSMetaAdapter/releases/download/1.0.2/CloudXMetaAdapter.xcframework.zip",
+            url: "https://github.com/cloudx-xenoss/CloudXiOSMetaAdapter/releases/download/1.0.3/CloudXMetaAdapter.xcframework.zip",
             checksum: "db38ec5e7d8f08d749c7271b62353faf2bffe5a4e7e9bbb3d902f1f9ab375768"
         )
     ]
